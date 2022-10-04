ee0c1d28c9b752d5689a7f77b032b238c5a0a24c
RehamAfifi <de.reham.yousef@gmail.com>
2022-10-04 18:34:35 +0200

    ui-commit

---
+++ js/javascript-cdn.js
@@ -0,0 +1,23 @@
+     type="text/javascript"; src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/5.0.0/mdb.min.js"
+
+     src="js/anime.min.js"
+    // <!--   Core JS Files   -->
+src="../assets/js/core/jquery.min.js"; type="text/javascript"
+ src="../assets/js/core/popper.min.js" ;type="text/javascript"
+src="../assets/js/core/bootstrap.min.js" ;type="text/javascript"
+
+// <!--  Plugin for Switches, full documentation here: http://www.jque.re/plugins/version3/bootstrap.switch/ -->
+src="../assets/js/plugins/bootstrap-switch.js"
+
+// <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+ src="../assets/js/plugins/nouislider.min.js"; type="text/javascript"
+
+// <!--  Plugin for the DatePicker, full documentation here: https://github.com/uxsolutions/bootstrap-datepicker -->
+ src="../assets/js/plugins/bootstrap-datepicker.js"; type="text/javascript"
+
+// <!--  Google Maps Plugin    -->
+ src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"
+
+
+// <!-- Control Center for Now Ui Kit: parallax effects, scripts for the example pages etc -->
+ src="../assets/js/now-ui-kit.js?v=1.2.0" ;type="text/javascript"
